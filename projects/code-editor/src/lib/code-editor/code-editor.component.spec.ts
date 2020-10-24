94dba67e9904fd441bc236dd85765d49202ad3ef
Denys Vuika <denys.vuika@gmail.com>
2020-10-24 18:04:50 +0100

    update tests

--- projects/code-editor/src/lib/code-editor/code-editor.component.spec.ts
+++ projects/code-editor/src/lib/code-editor/code-editor.component.spec.ts
@@ -1,26 +1,20 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CodeEditorComponent } from './code-editor.component';
 import { CodeEditorService } from '../services/code-editor.service';
 import { TypescriptDefaultsService } from '../services/typescript-defaults.service';
 import { JavascriptDefaultsService } from '../services/javascript-defaults.service';
+import { CodeEditorModule } from '../code-editor.module';
 
 describe('CodeEditorComponent', () => {
   let component: CodeEditorComponent;
   let fixture: ComponentFixture<CodeEditorComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [CodeEditorComponent],
-      providers: [
-        CodeEditorService,
-        TypescriptDefaultsService,
-        JavascriptDefaultsService
-      ]
-    }).compileComponents();
-  }));
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [CodeEditorModule.forRoot()],
+    });
+
     fixture = TestBed.createComponent(CodeEditorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
