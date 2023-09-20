07e899d28f790fb816f1d61ec2cd6feeda9b40a2
Denys Vuika <denys.vuika@gmail.com>
2023-09-20 09:24:33 +0100

    add SQL example

--- src/app/code-editor-demo/file-database.ts
+++ src/app/code-editor-demo/file-database.ts
@@ -8,6 +8,46 @@ const FILES_DATA: FileNode[] = [
     type: FileNodeType.folder,
     children: [
       {
+        name: 'schema.sql',
+        type: FileNodeType.file,
+        code: {
+          language: 'sql',
+          uri: 'schema.sql',
+          value: [
+            'CREATE TABLE dbo.EmployeePhoto (',
+            '  EmployeeId INT NOT NULL PRIMARY KEY,',
+            '  Photo VARBINARY(MAX) FILESTREAM NULL,',
+            '  MyRowGuidColumn UNIQUEIDENTIFIER NOT NULL ROWGUIDCOL UNIQUE DEFAULT NEWID()',
+            ');'
+          ].join('\n')
+        }
+      },
+      {
+        name: 'component.style.css',
+        type: FileNodeType.file,
+        code: {
+          language: 'css',
+          uri: 'component.style.css',
+          value: [
+            'html {',
+            '  background-color: #e2e2e2;',
+            '  margin: 0;',
+            '  padding: 0;',
+            '}',
+            '',
+            'body {',
+            '  background-color: #fff;',
+            '  border-top: solid 10px #000;',
+            '  color: #333;',
+            '  font-size: .85em;',
+            '  font-family: "Segoe UI","HelveticaNeue-Light", sans-serif;',
+            '  margin: 0;',
+            '  padding: 0;',
+            '}'
+          ].join('\n')
+        }
+      },
+      {
         name: 'json.json',
         type: FileNodeType.file,
         code: {
@@ -76,31 +116,6 @@ const FILES_DATA: FileNode[] = [
             '  constructor(translate: TranslateService) {',
             '    ',
             '  }',
-            '}'
-          ].join('\n')
-        }
-      },
-      {
-        name: 'component.style.css',
-        type: FileNodeType.file,
-        code: {
-          language: 'css',
-          uri: 'component.style.css',
-          value: [
-            'html {',
-            '  background-color: #e2e2e2;',
-            '  margin: 0;',
-            '  padding: 0;',
-            '}',
-            '',
-            'body {',
-            '  background-color: #fff;',
-            '  border-top: solid 10px #000;',
-            '  color: #333;',
-            '  font-size: .85em;',
-            '  font-family: "Segoe UI","HelveticaNeue-Light", sans-serif;',
-            '  margin: 0;',
-            '  padding: 0;',
             '}'
           ].join('\n')
         }
